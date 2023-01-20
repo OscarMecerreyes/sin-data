a1f82776a6445e1eb48673a8616eb8db97d0c95e
OscarMecerreyes <7836049@ALUMNOS.UFV.ES>
2023-01-20 12:23:13 +0100

    El vadin compila
    compila info

---
+++ skeleton-starter-flow-spring-23/skeleton-starter-flow-spring-23/src/main/java/org/vaadin/example/PokemonService.java
@@ -0,0 +1,11 @@
+package org.vaadin.example;
+
+import java.io.Serializable;
+import org.springframework.stereotype.Service;
+    @Service
+    public class PokemonService implements Serializable {
+        public String leePokemon(String tipoPeticion) {
+            return "Hello " + tipoPeticion;
+        }
+    }
+
